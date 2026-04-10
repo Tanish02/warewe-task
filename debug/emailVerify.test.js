d95f7feb7e6628feadb1ad070fdcf6102a7e023f
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-04-10 16:02:17 +0530

    add : test cases

---
+++ debug/emailVerify.test.js
@@ -0,0 +1,26 @@
+const validateEmail = require("../src/validator");
+
+test("valid email format", () => {
+  expect(validateEmail("user@gmail.com")).toBe(true);
+}); // valid email
+
+// if invalid email
+test("invalid email format", () => {
+  expect(validateEmail("usergmail.com")).toBe(false);
+});
+
+// if string is empty
+test("empty email", () => {
+  expect(validateEmail("")).toBe(false);
+});
+
+test("multiple @", () => {
+  expect(validateEmail("a@@gmail.com")).toBe(false);
+});
+
+// typo error / detection
+const getDidYouMean = require("../src/typoError");
+test("detect gmial typo", () => {
+  const suggestion = getDidYouMean("user@gmial.com");
+  expect(suggestion).toBe("user@gmail.com");
+});
